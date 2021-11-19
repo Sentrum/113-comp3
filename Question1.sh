0a6bb681d15323843e12f1b995af0505fde99011
regn <regn@mail.com>
2021-11-19 14:20:09 +0100

    updatess

--- Question1.sh
+++ Question1.sh
@@ -2,9 +2,9 @@
 
 mkdir $1
 cd $1
-x=1
+x=1;
 while [ "$x" -le "$2" ]
 do
-	mkdir Question$x
-	x=$(( $x + 1))
+	mkdir Question$x;
+	x=$(( $x + 1));
 done
--- Question3.sh
+++ Question3.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #script that ouputs the name of every folder, starting from the current directory and searching recursively
 
+#sick 1 liner:
+find . -type d
+
+
+#didnt work
 for f in *; do
     if [ -d "$f" ]; then
         echo $f
